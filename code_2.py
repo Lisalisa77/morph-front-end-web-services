dac7380d8dc41d6550f7d13eecd3a4b0d75fd0af
Lisa <143336008+Lisalisa77@users.noreply.github.com>
2023-10-30 05:59:53 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+    });
+
+}
+    const data = [];
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+
+    const data = generateRandomData();
+main();
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+}
+    }
+function generateRandomData() {
+function main() {
+    data.forEach(item => {
